b8fc00e39f766be9986a0d4ad8e6ebf9b8d6bf91
je3songli <58784080+je3songli@users.noreply.github.com>
2023-05-27 16:09:07 -0400

    Completed tasks on BagMain.java, not tested

--- src/main/java/BagMain.java
+++ src/main/java/BagMain.java
@@ -16,6 +16,12 @@ class BagMain {
      */
     public static void enhanceBags(Bag[] bags, boolean double_enhance_handbags) {
         // TODO: Implement this.
+        for (Bag b : bags) {
+            b.enhance();
+            if (double_enhance_handbags) {
+                b.enhance();
+            }
+        }
     }
 
     /**
@@ -29,5 +35,13 @@ class BagMain {
      */
     public static int countCrossbodyStraps(Bag[] bags) {
         // TODO: Implement this.
+        int totalStraps = 0;
+        for (Bag b : bags) {
+            if (b instanceof CrossbodyBag) {
+                totalStraps = totalStraps + ((CrossbodyBag) b).getNumberOfStraps();
+            }
+        }
+        return totalStraps;
+
     }
 }
